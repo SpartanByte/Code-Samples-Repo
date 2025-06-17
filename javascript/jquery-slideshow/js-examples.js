ae75ae658f944e7b62680fc009cce58c62ca2389
Brian Wardwell <briwar10@outlook.com>
2025-06-17 07:57:53 -0500

    add sass and javascript

---
+++ javascript/jquery-slideshow/js-examples.js
@@ -0,0 +1,23 @@
+// jQuery Image Slideshow
+$("#slideshow > div:first").hide();
+
+setInterval(function(){
+
+	$('#slideshow > div:first')
+		.fadeOut(2000)
+		.next()
+		.fadeIn(2000)
+		.end()
+		.appendTo('#slideshow');
+		
+}, 4000);
+	
+/* centering the images with extra WxH for the text below the images */
+jQuery.fn.setDivSize = function(extraHeight, extraWidth){
+	var img = new Image();
+	img.src = this.children('img').attr('src');
+	
+	this.height(img.height + extraHeight)
+		.width (img.width + extraHeight);
+	return this;
+}
---
+++ javascript/jquery-slideshow/slideshow.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.default')
+@section('description', 'slideshow example using jQuery, JavaScript and Laravel')
+@section('title', 'jQuery: Slideshow Example')
+
+@section('content')
+  <div class="static-content">
+  	<div id="gallery-thumbnails" class="slideshow" style="padding-bottom:15px;">
+      <h1 class="page-header">jQuery Slideshow Example</h1>
+{{--       	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script> --}}
+        <div id="slideshow">
+          <div class="picture">
+            <img src="/images/slide/edited/california-highway-mountains.jpg" alt="California Highway"><br>
+          </div>
+          <div class="picture">
+            <img src="/images/slide/edited/wet-highway.jpg" alt="Wet Highway"><br>
+          </div>
+          <div class="picture">
+            <img src="/images/slide/edited/highway-in-shadows.jpg" alt="Highway In Shadows"><br>
+          </div>
+          <div class="picture">
+          	<img src="/images/slide/edited/dock-at-sunset.jpg" alt="Dock At Sunset"><br>
+        	</div>
+          <div class="picture">
+            <img src="/images/slide/edited/ocean-dock.jpg" alt="Ocean Dock"><br>
+        </div>
+          <div class="picture">
+            <img src="/images/slide/edited/dock-in-weeds.jpg" alt="Dock In Weeds"><br>
+          </div>
+      </div>
+      <div id="gallery-clear-bottom"><button type="button" class="btn-primary btn-lg home-redirect"><a href="#">Back To Home</a></button>
+      <button type="button" class="btn-primary btn-lg home-redirect"><a href="#">Back To Home</a></button></div>
+    
+  {{-- js-examples.js --}}
+  <script src="{{ URL::asset('js/js-examples.js') }}" /></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+@stop
+
+
